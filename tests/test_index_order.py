93c7cf3ba0dc51f0968d7c672d89f35297e57e6a
Edouard Lavaud <e.lavaud@unyc.io>
2022-01-28 17:02:09 +0100

    add zyxel_os_sys_atsh

--- tests/test_index_order.py
+++ tests/test_index_order.py
@@ -53,6 +53,7 @@ OS_CHOICES = [
     "vyos",
     "watchguard_firebox",
     "yamaha",
+    "zyxel_os",
 ]
 CHOICES_STRING = "|".join(OS_CHOICES)
 RE_TEMPLATE_OS = re.compile(rf"^({CHOICES_STRING})")
